3e90696f772a4c5a4f5576a60a2cab8bdf4f5c19
anzarsh <anzar.terek@gmail.com>
2017-02-17 14:46:39 +0300

    sdf

--- app/js/common.js
+++ app/js/common.js
@@ -20,7 +20,7 @@ $(function() {
         $(this).toggleClass('az-close').next('.az-menu').slideToggle(300);
     });
     $('.az-menu>li>a').click(function(){
-        if(!window.matchMedia( "(min-width: 768px)" ).matches){
+        if(window.matchMedia( "(max-width: 768px)" ).matches){
             if($(this).next('ul').length>0&&$(this).next('ul').css('display')=='none'){
                 $('.az-menu>li>a').removeClass('active')
                 $('.az-menu>li>a+ul').removeClass('active').slideUp(300);
@@ -30,7 +30,7 @@ $(function() {
                 $(this).toggleClass('active').next('ul').slideToggle(300);
                 return false;
             }
-        } else {
+        } else if (window.matchMedia( "(min-width: 768px)" ).matches && window.matchMedia( "(max-width: 1200px)" ).matches){
             if($(this).hasClass('active')){
                 $(this).removeClass('active');
                 return false;
